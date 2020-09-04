fe7ae020404e76a6873a33059287b337cf77d407
imshmen <imshmen077@gmail.com>
2020-09-04 19:04:46 +0600

    comment styled

--- Frontend/components/home/post.jsx
+++ Frontend/components/home/post.jsx
@@ -9,6 +9,7 @@ import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
+import { colors } from "@material-ui/core";
 import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
@@ -16,6 +17,8 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { useForm } from "react-hook-form";
 import { useCookies } from "react-cookie";
+import { Grid } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,9 +36,28 @@ const useStyles = makeStyles((theme) => ({
     transform: "rotate(180deg)",
   },
   avatar: {
-    backgroundColor: red[500],
+    //backgroundColor: red[500],
+    color: "white",
+  },
+  avatarComment: {
+    color: "white",
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    //backgroundColor: red[500],
+    fontSize: 12,
   },
 }));
+
+const avatarColor = [
+  colors.red[500],
+  colors.pink[500],
+  colors.purple[500],
+  colors.blue[500],
+  colors.teal[500],
+  colors.orange[500],
+  colors.orange[500],
+  colors.brown[500],
+];
 
 const Post = ({ post }) => {
   const classes = useStyles();
@@ -65,13 +87,20 @@ const Post = ({ post }) => {
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={<Avatar className={classes.avatar}>স্ব</Avatar>}
+        avatar={
+          <Avatar
+            className={classes.avatar}
+            style={{ backgroundColor: avatarColor[post.author[0].charCodeAt(0) % avatarColor.length] }}
+          >
+            {post.author[0]}
+          </Avatar>
+        }
         action={
           <IconButton>
             <MoreVertIcon />
           </IconButton>
         }
-        title={post.author}
+        title={<b>{post.author}</b>}
         subheader="September ১৪, ২০১৬"
       />
 
@@ -86,14 +115,13 @@ const Post = ({ post }) => {
           <FavoriteIcon />
         </IconButton>
 
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-        >
-          <ExpandMoreIcon />
+        <IconButton style={{ marginLeft: "auto" }} onClick={handleExpandClick} aria-expanded={expanded}>
+          <Typography variant="body2">মন্তব্যসমূহ</Typography>
+          <ExpandMoreIcon
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+          />
         </IconButton>
       </CardActions>
 
@@ -102,21 +130,36 @@ const Post = ({ post }) => {
           {post.comments
             ? post.comments.map((comment) => {
                 return (
-                  <li key={comment._id}>
-                    {comment.author} {"->"} {comment.content}
-                  </li>
+                  <React.Fragment>
+                    <Grid container key={comment._id} style={{ marginBottom: 5 }}>
+                      <Grid item xs={1}>
+                        <Avatar
+                          className={classes.avatarComment}
+                          style={{ backgroundColor: avatarColor[comment.author[0].charCodeAt(0) % avatarColor.length] }}
+                        >
+                          {comment.author[0]}
+                        </Avatar>
+                      </Grid>
+                      <Grid item xs={11}>
+                        <Typography>
+                          {<b>{comment.author}</b>} {comment.content}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </React.Fragment>
                 );
               })
             : null}
+
           <form onSubmit={handleSubmit(handleCommentSubmit)}>
             <TextField
               inputRef={register}
               variant="outlined"
               margin="normal"
-              required
               fullWidth
               name="comment"
-              label="comment"
+              placeholder="মতামত দিন..."
+              margin="dense"
             />
           </form>
         </CardContent>
